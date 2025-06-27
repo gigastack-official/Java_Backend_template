2ddee6fa31ba5fcb7297270c872745838f67c78a
Vladimir Gorohov <vova-gorohov04@mail.ru>
2025-06-27 18:20:21 +0700

    swagger config

---
+++ src/main/java/ru/gigastack/backend/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package ru.gigastack.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        final String securitySchemeName = "BearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OwnHack1 API")
+                        .description("Документация к backend API проекта OwnHack1")
+                        .version("v1.0"))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+    }
+}
